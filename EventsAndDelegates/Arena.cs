7dfb53f5f8c1ea3aebfc0ca5ca3173b5d03fab8b
Erik Josander <josander.erik@gmail.com>
2021-06-14 14:52:13 +0200

    Warrior, Arena and Weapon
    Added more classes to the project

---
+++ EventsAndDelegates/Arena.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventsAndDelegates
+{
+    public class Arena
+    {
+        public Arena(string name, List<Warrior> warriors)
+        {
+            Name = name;
+            Warriors = warriors;
+        }
+        public string Name { get; set; }
+        public List<Warrior> Warriors { get; set; }
+    }
+}
---
+++ EventsAndDelegates/Weapon.cs
@@ -0,0 +1,15 @@
+﻿namespace EventsAndDelegates
+{
+    public class Weapon
+    {
+        public Weapon(string name, double damage, int hitRate)
+        {
+            Name = name;
+            Damage = damage;
+            HitRate = hitRate;
+        }
+        public string Name { get; set; }
+        public double Damage { get; set; }
+        public int HitRate { get; set; }
+    }
+}
